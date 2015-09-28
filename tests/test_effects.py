40f73e9fe332b85fea64517e174c89e9aacc53fb
Carl Sobolewski <sobolews@ualberta.ca>
2015-09-28 00:06:59 -0600

    Fix test_all_effects_have_source
    self.all_effects was previously not being populated, so the test didn't catch
    any missing sources.

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -6,11 +6,15 @@ from pokedex import effects, abilities, statuses, weather, baseeffect
 class TestEffectDefinitions(TestCase):
     def setUp(self):
         self.all_effects = [effect_cls for module in (effects, abilities, statuses, weather)
-                            for effect_cls in vars(module).items() if
+                            for effect_cls in vars(module).values() if
                             inspect.isclass(effect_cls) and
                             issubclass(effect_cls, baseeffect.BaseEffect) and
-                            effect_cls is not baseeffect.BaseEffect]
+                            effect_cls not in (baseeffect.BaseEffect,
+                                               effects.BaseAuraFieldEffect,
+                                               effects.MagicBounceBase,
+                                               statuses.BaseStatusEffect,
+                                               weather.BaseWeatherEffect)]
 
     def test_all_effects_have_source(self):
         for effect in self.all_effects:
-            self.assertIsNotNone(effect.source)
+            self.assertIsNotNone(effect.source, msg='%s.source is None' % effect)
