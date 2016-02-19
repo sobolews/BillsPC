616af7e5457c3e8d9ac1e1cb2fb71334e2aba232
Carl Sobolewski <sobolews@ualberta.ca>
2016-02-19 12:22:02 -0700

    Use activate_effect for on_switch_out

--- battle/battleengine.py
+++ battle/battleengine.py
@@ -734,8 +734,7 @@ class BattleEngine(object):
     def switch_out(self, outgoing, incoming):
         outgoing.is_switching_out = True
 
-        for effect in sorted(outgoing.effects, key=lambda e: -e.on_switch_out.priority):
-            effect.on_switch_out(outgoing, incoming, self)
+        outgoing.activate_effect('on_switch_out', outgoing, incoming, self)
 
         outgoing.clear_effects(self)
         outgoing.boosts = Boosts()
