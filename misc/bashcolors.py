3b8d395338d62e78ae3dcf32d9dd9181e76cfa46
Carl Sobolewski <sobolews@ualberta.ca>
2016-05-23 14:18:17 -0600

    bashcolors: increase brightness of sent() text

--- misc/bashcolors.py
+++ misc/bashcolors.py
@@ -2,7 +2,7 @@ def received(s):
     return '\033[38;5;213m\033[2m%s\033[0m' % s
 
 def sent(s):
-    return '\033[38;5;2m\033[2m%s\033[0m' % s
+    return '\033[38;5;2m%s\033[0m' % s
 
 def active(s):
     return '\033[38;5;83m\033[2m%s\033[0m' % s
