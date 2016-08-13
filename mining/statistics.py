a012fbc8e9a36093c94979b5110e2e4791199ffa
Carl Sobolewski <sobolews@ualberta.ca>
2016-08-13 15:43:14 -0600

    rbstats: lookup by mega forme if pokemon is mega

--- mining/statistics.py
+++ mining/statistics.py
@@ -264,7 +264,7 @@ def rbstats_key(battlepokemon):
     """
     Get the rbstats key associated with a BattlePokemon. For use by clients of RandbatsStatistics.
     """
-    if battlepokemon.can_mega_evolve:
+    if battlepokemon.can_mega_evolve or battlepokemon.is_mega:
         return battlepokemon.item.forme
     else:
         return '%sL%d' % (battlepokemon.base_species, battlepokemon.level)
