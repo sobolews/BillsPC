f27df7f93d72b45fa49675fbb5eb6355eebbfb8f
Carl Sobolewski <sobolews@ualberta.ca>
2016-08-17 00:36:49 -0600

    Move EnumMeta, NoCopy, EnumBase to misc/enum.py

---
+++ misc/enum.py
@@ -0,0 +1,14 @@
+class EnumMeta(type):
+    """ Enum metaclass. Creates a values dictionary with the attribute name as its own value """
+    def __new__(mcs, name, bases, dct):
+        for val in dct:
+            dct[val] = val
+        dct['values'] = {k: v for k, v in dct.items() if not k.startswith('__')}
+        return type.__new__(mcs, name, bases, dct)
+
+class NoCopy(object):
+    def __deepcopy__(self, memo):
+        return self
+
+class EnumBase(NoCopy):
+    __metaclass__ = EnumMeta
