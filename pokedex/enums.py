15ac7ce3e702f8e3167ea5f5063455a4f017cc2d
Carl Sobolewski <sobolews@ualberta.ca>
2016-08-18 22:58:15 -0600

    enums: define each value separately to avoid requiring count
    The tuple-unpacking trick before is too ugly because it requires an
    up-to-date count for each enum.

--- pokedex/enums.py
+++ pokedex/enums.py
@@ -1,44 +1,129 @@
 from misc.enum import BaseEnum, NoCopy
 
 class Type(BaseEnum):
-    (NORMAL, FIGHTING, FLYING, POISON, GROUND, ROCK,
-     BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC,
-     PSYCHIC, ICE, DRAGON, DARK, FAIRY, NOTYPE) = [()]*19
+    NORMAL = ()
+    FIGHTING = ()
+    FLYING = ()
+    POISON = ()
+    GROUND = ()
+    ROCK = ()
+    BUG = ()
+    GHOST = ()
+    STEEL = ()
+    FIRE = ()
+    WATER = ()
+    GRASS = ()
+    ELECTRIC = ()
+    PSYCHIC = ()
+    ICE = ()
+    DRAGON = ()
+    DARK = ()
+    FAIRY = ()
+    NOTYPE = ()
 
 class Status(BaseEnum):
-    BRN, FNT, FRZ, PAR, PSN, SLP, TOX = [()]*7
+    BRN = ()
+    FNT = ()
+    FRZ = ()
+    PAR = ()
+    PSN = ()
+    SLP = ()
+    TOX = ()
 
 class Volatile(BaseEnum):
-    (ATTRACT, AUTOTOMIZE, BATONPASS, CHOICELOCK, CONFUSE, DESTINYBOND,
-     DISABLE, ENCORE, FLASHFIRE, FLINCH, FORECAST, GEM, KINGSSHIELD,
-     LEECHSEED, LOCKEDMOVE, MAGICCOAT, MAGNETRISE, PARENTALBOND,
-     PARTIALTRAP, PERISHSONG, PIROUETTE, PROTECT, PURSUIT, ROOST,
-     SHEERFORCE, SLOWSTART, SPIKYSHIELD, STALL, SUBSTITUTE, TAUNT,
-     TRANSFORMED, TRAPPED, TRUANT, TWOTURNMOVE, UNBURDEN, VANISHED, YAWN) = [()]*37
+    ATTRACT = ()
+    AUTOTOMIZE = ()
+    BATONPASS = ()
+    CHOICELOCK = ()
+    CONFUSE = ()
+    DESTINYBOND = ()
+    DISABLE = ()
+    ENCORE = ()
+    FLASHFIRE = ()
+    FLINCH = ()
+    FORECAST = ()
+    GEM = ()
+    KINGSSHIELD = ()
+    LEECHSEED = ()
+    LOCKEDMOVE = ()
+    MAGICCOAT = ()
+    MAGNETRISE = ()
+    PARENTALBOND = ()
+    PARTIALTRAP = ()
+    PERISHSONG = ()
+    PIROUETTE = ()
+    PROTECT = ()
+    PURSUIT = ()
+    ROOST = ()
+    SHEERFORCE = ()
+    SLOWSTART = ()
+    SPIKYSHIELD = ()
+    STALL = ()
+    SUBSTITUTE = ()
+    TAUNT = ()
+    TRANSFORMED = ()
+    TRAPPED = ()
+    TRUANT = ()
+    TWOTURNMOVE = ()
+    UNBURDEN = ()
+    VANISHED = ()
+    YAWN = ()
 
 class SideCondition(BaseEnum):
-    HEALINGWISH, LIGHTSCREEN, REFLECT, SAFEGUARD, TAILWIND, WISH = [()]*6
+    HEALINGWISH = ()
+    LIGHTSCREEN = ()
+    REFLECT = ()
+    SAFEGUARD = ()
+    TAILWIND = ()
+    WISH = ()
 
 class Hazard(BaseEnum):
-    TOXICSPIKES, SPIKES, STEALTHROCK, STICKYWEB = [()]*4
+    TOXICSPIKES = ()
+    SPIKES = ()
+    STEALTHROCK = ()
+    STICKYWEB = ()
 
 class PseudoWeather(BaseEnum):
-    AURABREAK, DARKAURA, ELECTRICTERRAIN, FAIRYAURA, TRICKROOM = [()]*5
+    AURABREAK = ()
+    DARKAURA = ()
+    ELECTRICTERRAIN = ()
+    FAIRYAURA = ()
+    TRICKROOM = ()
 
 class Weather(BaseEnum):
-    (DELTASTREAM, DESOLATELAND, HAIL, PRIMORDIALSEA,
-     RAINDANCE, SANDSTORM, SUNNYDAY) = [()]*7
+    DELTASTREAM = ()
+    DESOLATELAND = ()
+    HAIL = ()
+    PRIMORDIALSEA = ()
+    RAINDANCE = ()
+    SANDSTORM = ()
+    SUNNYDAY = ()
 Weather.TRIO = (Weather.PRIMORDIALSEA, Weather.DESOLATELAND, Weather.DELTASTREAM)
 
 class Cause(BaseEnum):
-    (CONFUSE, CRASH, DIRECT, DRAIN, HAZARD, MOVE,
-     OTHER, RECOIL, RESIDUAL, SELFDESTRUCT, WEATHER) = [()]*11
+    CONFUSE = ()
+    CRASH = ()
+    DIRECT = ()
+    DRAIN = ()
+    HAZARD = ()
+    MOVE = ()
+    OTHER = ()
+    RECOIL = ()
+    RESIDUAL = ()
+    SELFDESTRUCT = ()
+    WEATHER = ()
 
 class Decision(BaseEnum):
-    MOVE, MEGAEVO, POSTSWITCH, RESIDUAL, SWITCH = [()]*5
+    MOVE = ()
+    MEGAEVO = ()
+    POSTSWITCH = ()
+    RESIDUAL = ()
+    SWITCH = ()
 
 class MoveCategory(BaseEnum):
-    PHYSICAL, SPECIAL, STATUS = [()]*3
+    PHYSICAL = ()
+    SPECIAL = ()
+    STATUS = ()
 STATUS, PHYSICAL, SPECIAL = MoveCategory.STATUS, MoveCategory.PHYSICAL, MoveCategory.SPECIAL
 
 class SingletonEnum(NoCopy):
