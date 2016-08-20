dc763a8ae08556953ab87cc739c3e33c8bb57512
Carl Sobolewski <sobolews@ualberta.ca>
2016-08-20 00:56:19 -0600

    AI: implement RandomAgent

--- AI/agents.py
+++ AI/agents.py
@@ -1,3 +1,4 @@
+import random
 from AI.enums import Policy
 
 
@@ -16,7 +17,18 @@ class BaseAgent(object):
 
 
 class RandomAgent(BaseAgent):
-    pass
+    def __init__(self, switch_freq=0.1):
+        self.switch_freq = switch_freq
+
+    def __repr__(self):
+        return '<RandomAgent:switch_freq=%s>' % self.switch_freq
+
+    def select_action(self, _, moves, switches, can_mega):
+        if not moves or (switches and random.random() < self.switch_freq):
+            return random.choice(switches), False
+
+        mega = can_mega and random.choice((True, False))
+        return random.choice(moves), mega
 
 
 class MatrixAgent(BaseAgent):
