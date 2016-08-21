3e41daec86ae0c95ca7370d19b6665e549c84616
Carl Sobolewski <sobolews@ualberta.ca>
2016-08-21 15:02:22 -0600

    install.sh: generate rbstats.pkl at install time

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -ev
+set -ex
 pip install -r requirements.txt
 git submodule update --init
 (cd showdowndata/Pokemon-Showdown && npm install --production)
 cp showdowndata/js/* showdowndata/Pokemon-Showdown
 cp showdowndata/Pokemon-Showdown/config/config-example.js showdowndata/Pokemon-Showdown/config/config.js
-echo "Dependencies installed. Run ./test.sh to test"
+(set +x; echo 'Dependencies installed.' \
+              'Generating randbats statistics file (rbstats.pkl) with 5000 teams.' \
+              'For better results, you should mine for at least 100,000 teams with' \
+              '`./BillsPC.py mine 100000`') 2>/dev/null
+./BillsPC.py mine 5000
+(set +x; echo 'Done. Run `./test.sh` to test') 2>/dev/null
