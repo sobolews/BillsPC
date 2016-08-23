136195b99992a36cefeadcfc32280626df0f2f37
Carl Sobolewski <sobolews@ualberta.ca>
2016-08-22 22:25:09 -0600

    AI: expose a method to set my_player attribute in BaseAgent

--- AI/baseagent.py
+++ AI/baseagent.py
@@ -3,6 +3,12 @@ from abc import ABCMeta, abstractmethod
 class BaseAgent(object):
     __metaclass__ = ABCMeta
 
+    def __init__(self):
+        self.my_player = None
+
+    def set_my_player(self, player):
+        self.my_player = player
+
     @abstractmethod
     def select_action(self, battlefield, moves, switches, can_mega):
         pass
