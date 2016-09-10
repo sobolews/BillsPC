3da513d25c419fc8bf025e3ffd03cd85a9e34400
Carl Sobolewski <sobolews@ualberta.ca>
2016-09-10 01:10:08 -0600

    AI: Add BattleState enum

--- AI/enums.py
+++ AI/enums.py
@@ -3,3 +3,12 @@ from misc.enum import BaseEnum
 class Strategy(BaseEnum):
     RANDOM = ()
     MATRIX = ()
+
+class BattleState(BaseEnum):
+    NEW_BATTLE = ()
+    NEW_TURN = ()
+    MUST_SWITCH = ()
+    POST_FAINT_SWITCH = ()
+    DOUBLE_POST_FAINT_SWITCH = ()
+    CHANCE_ACCURACY = ()
+    CHANCE_SECONDARY = ()
