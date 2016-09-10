a8624c8210ebe35fcd0ddc9ef69c3618085b362c
Carl Sobolewski <sobolews@ualberta.ca>
2016-09-10 01:07:03 -0600

    AI: add make_events method to actions
    action.make_events will create a MoveEvent [+MegaEvoEvent], or a
    SwitchEvent suitable for use in a Battle.

--- AI/actions.py
+++ AI/actions.py
@@ -1,4 +1,6 @@
 from battle.enums import Decision
+from battle.events import MoveEvent, SwitchEvent, MegaEvoEvent
+from battle.moves import movedex
 
 class Action(object):
     @property
@@ -16,8 +18,18 @@ class MoveAction(Action):
     def command_string(self):
         return '/choose move %d' % self.index
 
+    def make_events(self, user, side, battle):
+        events = []
+        move = movedex[self.move_name]
+        events.append(MoveEvent(user, battle.effective_spe(user),
+                                battle.modify_priority(user, move), move))
+        if user.can_mega_evolve:
+            events.append(MegaEvoEvent(user, battle.effective_spe(user)))
+
+        return events
+
     def __repr__(self):
-        return '(move: %s)' % self.move_name
+        return '<move: %s>' % self.move_name
 
 class SwitchAction(Action):
     action_type = Decision.SWITCH
@@ -30,5 +42,12 @@ class SwitchAction(Action):
     def command_string(self):
         return '/choose switch %d' % self.index
 
+    def make_events(self, active, side, battle, check_spe=True):
+        incoming = next(teammate for teammate in side.team if teammate.name == self.incoming_name)
+        assert not incoming.is_fainted()
+
+        event = SwitchEvent(active, battle.effective_spe(active) if check_spe else 0, incoming)
+        return [event]
+
     def __repr__(self):
-        return '(switch: %s)' % self.incoming_name
+        return '<switch: %s>' % self.incoming_name
